6d017a3cbd113faf4575703b8b71b25b50ed8732
hcdzxr <zhuxinrong@newsun.co.jp>
2025-07-26 16:46:06 +0800

    remove mockjs

--- src/main.js
+++ src/main.js
@@ -27,10 +27,10 @@ import "simplebar"; // JS，使 data-simplebar 自动生效
  * Currently MockJs will be used in the production environment,
  * please remove it before going online ! ! !
  */
-if (process.env.NODE_ENV === "production") {
-  const { mockXHR } = require("../mock");
-  mockXHR();
-}
+// if (process.env.NODE_ENV === "production") {
+//   const { mockXHR } = require("../mock");
+//   mockXHR();
+// }
 
 // set ElementUI lang to EN
 Vue.use(ElementUI, { locale });
