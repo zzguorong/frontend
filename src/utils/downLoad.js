fbd3c61b35725eb7cdda8185bcf4a8b91449ec51
hcdzxr <zhuxinrong@newsun.co.jp>
2025-07-26 19:08:10 +0800

    updated download method

--- src/utils/downLoad.js
+++ src/utils/downLoad.js
@@ -3,21 +3,10 @@ export function downloadPNG(imgsrc) {
   if (process.env.NODE_ENV === 'development') {
     imgsrc = imgsrc.replace('https://api.gaiass.com', '');
   }
-  this.$message.success('开始下载JPG格式...');
-  var image = new Image();
-  image.setAttribute('crossOrigin', 'anonymous');
-  image.onload = function() {
-    var canvas = document.createElement('canvas');
-    canvas.width = image.width;
-    canvas.height = image.height;
-    var context = canvas.getContext('2d');
-    context.drawImage(image, 0, 0, image.width, image.height);
-    var url = canvas.toDataURL('image/png');
-    var a = document.createElement('a');
-    var event = new MouseEvent('click');
-    a.download = `image${Date.now()}.png`;
-    a.href = url;
-    a.dispatchEvent(event);
-  };
-  image.src = imgsrc;
+  const link = document.createElement('a');
+  link.href = imgsrc;
+  link.download = `image_${Date.now()}.png`;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
