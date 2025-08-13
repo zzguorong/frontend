0eeda8d49c7ce0f2cc28e5290e7a1147d49ec245
hcdzxr <zhuxinrong@newsun.co.jp>
2025-08-13 16:53:41 +0800

    try to fixed navigation back issue

--- src/views/loginInterface/termOfservice.vue
+++ src/views/loginInterface/termOfservice.vue
@@ -151,7 +151,11 @@ export default {
   name: 'TermOfservice',
   methods: {
     close() {
-      this.$router.push('/');
+      if (window.history.state.back === null || window.history.length <= 1) {
+        this.$router.push('/');
+      } else {
+        this.$router.go(-1);
+      }
     }
   }
 };
